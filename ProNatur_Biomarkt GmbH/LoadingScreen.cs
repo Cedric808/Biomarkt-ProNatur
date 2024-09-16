81f9b85bc2779ceb68d8ca44c22b517c3b68c56c
ES-AREA1\C7VA <CedricEckermann@eurofins.de>
2024-09-16 08:52:22 +0200

    initial biomarktprojekt

---
+++ ProNatur_Biomarkt GmbH/LoadingScreen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ProNatur_Biomarkt_GmbH
+{
+    public partial class LoadingScreen : Form
+    {
+
+        private int loadingBarValue;
+        public LoadingScreen()
+        {
+            InitializeComponent();
+        }
+        private void LoadingScreen_Load(object sender, EventArgs e)
+        {
+            loadingbarTimer.Start();
+        }
+
+        private void loadingbarTimer_Tick(object sender, EventArgs e)
+        {
+
+            loadingBarValue += 1;
+
+            lblLoadingProgress.Text = loadingBarValue.ToString() + "%";   //aus 1 = "1" | int to string
+            loadingProgressbar.Value = loadingBarValue;   //das selbe wie bei den Eigenschaften
+
+            if (loadingBarValue >= loadingProgressbar.Maximum) //damit haben wir keine magicnumber
+            {
+                loadingbarTimer.Stop();
+                //finish loadng
+            }
+
+        }
+
+       
+    }
+}
+//Lektion 105. minute 6,22
---
+++ ProNatur_Biomarkt GmbH/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("ProNatur_Biomarkt GmbH")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Eurofins")]
+[assembly: AssemblyProduct("ProNatur_Biomarkt GmbH")]
+[assembly: AssemblyCopyright("Copyright © Eurofins 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("a64bb449-855a-4d1f-897b-7fd99d77d542")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
